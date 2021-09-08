b1edc39fbfe3c11e0793ead747547d53099238ea
yuenu <yuenuni@hotmail.com>
2021-09-08 17:32:37 +0800

    feat: add search tern

---
+++ components/SearchTern.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <form class="search__form" @submit.prevent="submitForm">
+      <div class="search__group">
+        <input
+          v-model.trim="searchInput"
+          :class="['search__tern', { square: inputActive }]"
+          type="text"
+          :placeholder="t('search')"
+          autocapitalize="off"
+        />
+        <button
+          :class="['search__icon', { close: inputActive }]"
+          type="reset"
+          @click="formToggle"
+        ></button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component<SearchTern>({})
+export default class SearchTern extends Vue {}
+</script>
+
