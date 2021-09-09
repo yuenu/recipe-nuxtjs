f01fa3fe20066c44848d11d6d21a36ae99bccd26
yuenu <yuenuni@hotmail.com>
2021-09-09 09:46:19 +0800

    fix: error src

--- plugins/skeletonDirective.ts
+++ plugins/skeletonDirective.ts
@@ -4,14 +4,12 @@ Vue.directive('skeleton', {
   bind(el, binding) {
     function createImage() {
       const img = new Image()
-      if(binding.value) {
+      if(binding.value && binding.value.src) {
         const { src, alt } = binding.value
         img.setAttribute('alt', alt)
         img.setAttribute('src', src)
-      } else {
-        img.setAttribute('alt', 'image')
-        img.setAttribute('src', 'https://www.globalsign.com/application/files/9516/0389/3750/What_Is_an_SSL_Common_Name_Mismatch_Error_-_Blog_Image.jpg')
-      }
+      } 
+
       img.onload = function () {
         el.appendChild(img)
         el.classList.remove('loading')
