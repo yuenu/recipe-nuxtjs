255c1a55ee35e437caed54b325d660278b482f18
yuenu <yuenuni@hotmail.com>
2021-09-10 17:29:09 +0800

    feat: handle menu navigation

---
+++ components/HomeMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="menu">
+    <NuxtLink :to="localePath('/')">{{ $t('navigation.home') }}</NuxtLink>
+    
+  </section>
+</template>
+
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component<HomeMenu>({})
+export default class HomeMenu extends Vue {
+
+}
+</script>
